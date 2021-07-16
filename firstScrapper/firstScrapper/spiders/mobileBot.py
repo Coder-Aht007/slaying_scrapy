4cb0dbff680477e3bced73f3235663c6dac93918
AhtashamUlHasaan <ahtashamulhasaan@A006-00586.local>
2021-07-16 11:37:06 +0500

    whatMobileBotDone

--- firstScrapper/firstScrapper/spiders/mobileBot.py
+++ firstScrapper/firstScrapper/spiders/mobileBot.py
@@ -4,7 +4,14 @@ import scrapy
 class MobilebotSpider(scrapy.Spider):
     name = 'mobileBot'
     allowed_domains = ['https://www.whatmobile.com.pk']
-    start_urls = ['http://https://www.whatmobile.com.pk/']
+    start_urls = ['https://www.whatmobile.com.pk/']
 
     def parse(self, response):
-        pass
+        mobileName = response.xpath("//h4/a[contains(@class, 'BiggerText')]/@title").extract()
+        mobilePrice = response.xpath("//li/span[contains(@class, 'PriceFont')]/text()").extract()
+
+        for item in zip(mobileName, mobilePrice):
+            yield {
+                "name" : item[0],
+                "price" : item[1],
+            }
